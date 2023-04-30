b6501a8da75f15f5434dd1d4f6a1b9194aa59ed7
Hazel Chui <hazelchui@uchicago.edu>
2023-04-30 22:54:19 +0000

    77.14%

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -13,6 +13,85 @@ class PhotosController < ApplicationController
     matching_photos = Photo.where({ :id => url_id})
 
     @the_photo = matching_photos.at(0)
-    render({ :template => "photo_templates/index.html.erb"}) 
+    render({ :template => "photo_templates/show.html.erb"}) 
   end 
+
+  def baii
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    # render({ :template => "photo_templates/baii.html.erb"}) 
+
+    redirect_to("/photos")
+  end 
+
+  def create
+
+  input_image = params.fetch("query_image")
+
+  input_caption = params.fetch("query_caption")
+
+  input_owner_id = params.fetch("query_owner_id")
+
+  a_new_photo = Photo.new
+  a_new_photo.image = input_image
+  a_new_photo.caption = input_caption
+  a_new_photo.owner_id = input_owner_id
+
+  a_new_photo.save
+
+    # render({ :template => "photo_templates/create.html.erb"}) 
+
+  redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
+
+  def update
+
+    the_id = params.fetch("modify_id")
+
+    matching_photos = Photo.where({ :id => the_id})
+
+    the_photo = matching_photos.at(0)
+    
+    input_image = params.fetch("query_image")
+
+    input_caption = params.fetch("query_caption")
+
+    the_photo.image = input_image
+
+    the_photo.caption = input_caption
+
+    the_photo.save
+
+    # render({ :template => "photo_templates/update.html.erb"}) 
+
+    redirect_to("/photos/" + the_photo.id.to_s)
+  end 
+
+  def add
+
+    input_photo_id = params.fetch("query_photo_id")
+
+    input_author_id = params.fetch("query_author_id")
+
+    input_comment = params.fetch("query_comment")
+  
+    a_new_comment = Comment.new
+    a_new_comment.photo_id = input_photo_id
+    a_new_comment.author_id = input_author_id
+    a_new_comment.body = input_comment
+  
+    a_new_comment.save
+  
+      # render({ :template => "photo_templates/create.html.erb"}) 
+  
+    redirect_to("/photos/" + a_new_comment.photo_id.to_s)
+
+  end
+
 end 
